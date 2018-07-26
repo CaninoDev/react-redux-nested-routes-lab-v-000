8fac414525f95e96162a1308ed4aab8e0faf7cf2
Canino Developments <ian.canino@gmail.com>
2018-07-26 11:52:55 -0400

    Done.

--- src/containers/PetsPage.js
+++ src/containers/PetsPage.js
@@ -1,20 +1,28 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Route, Switch } from 'react-router-dom';
-import { fetchPets } from '../actions';
-import PetsNew from './PetsNew';
-import PetsShow from './PetsShow';
-import PetsList from '../components/PetsList';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Route, Switch } from 'react-router-dom'
+import { fetchPets } from '../actions'
+import PetsNew from './PetsNew'
+import PetsShow from './PetsShow'
+import PetsList from '../components/PetsList'
 
 class PetsPage extends Component {
-
-  componentDidMount() {
-    this.props.fetchPets();
+  componentDidMount () {
+    this.props.fetchPets()
   }
 
-  render() {
+  render () {
     return (
-      <div>Pets Page</div>
+      <div>
+        <PetsList pets={this.props.pets} />
+        <Switch>
+          <Route path={`${this.props.match.url}/new`} component={PetsNew} />
+          <Route path={`${this.props.match.url}/:petId`} component={PetsShow} />
+          <Route exact path={this.props.match.url} render={() => (
+            <h3>Please Select Pet from the List</h3>
+          )} />
+        </Switch>
+      </div>
     )
   }
 };
@@ -22,7 +30,7 @@ class PetsPage extends Component {
 const mapStateToProps = state => {
   return {
     pets: state.pets
-  };
+  }
 }
 
-export default connect(mapStateToProps, { fetchPets })(PetsPage);
+export default connect(mapStateToProps, { fetchPets })(PetsPage)
